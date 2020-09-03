23e208398b003dac5abf7fc770300f727a7149e5
Ge Yang <yangge1987@gmail.com>
2020-09-02 19:49:37 -0700

    add README.md

--- specs/README.py
+++ specs/README.py
@@ -1,4 +1,6 @@
 from cmx import doc
+import gym
+from cmx.backends.components import Image
 
 doc @ """
 # Extended Taskset for the Fetch Robot
@@ -7,22 +9,33 @@ doc @ """
 
 Existing Fetch environments from gym
 """
-doc.csv @ """
-Name,           Progress,  ID
-Reach-v2,       ✅ gym,     
-Push-v2,        ✅ gym,     
-PickPlace-v2,   ✅ gym,      
-Slide-v2,       ✅ gym,      
+
+
+def render(env_id):
+    env = gym.make(env_id)
+    env.reset()
+    img = env.render('rgb_array', width=120, height=80)
+    html = Image(img)._html
+    return html
+
+
+doc @ f"""
+  Name        | Render
+------------- | -------------------------
+  Reach-v2    | {render('FetchReach-v1')}
+  Push-v2     | {render('FetchPush-v1')}
+  PickPlace-v2| {render('FetchPickAndPlace-v1')}     
+  Slide-v2    | {render('FetchSlide-v1')}          
 """
 
 doc @ """
 New environments we want to build
 """
-doc.csv @ """
-Name,           Progress,  ID
-Bin-picking-v2,     ,      
-Box-open-v2,        ,      
-Box-close-v2,       ,      
-"""
+# doc.csv @ """
+# Name,           Progress,  ID
+# Bin-picking-v2,     ,
+# Box-open-v2,        ,
+# Box-close-v2,       ,
+# """
 
 doc.flush()
