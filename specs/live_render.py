5a8e49ab787e78835c6defd7de7157a7dcc6529a
Ge Yang <yangge1987@gmail.com>
2020-09-02 22:33:18 -0700

    add basic asset and robotic environment class

--- specs/live_render.py
+++ specs/live_render.py
@@ -9,7 +9,9 @@ def live_render(env_id):
         action = env.action_space.sample()
         timestep = env.step(action)
         env.render()
+        print(timestep)
         time.sleep(0.1)
+
 
 if __name__ == '__main__':
     live_render('FetchReach-v1')
